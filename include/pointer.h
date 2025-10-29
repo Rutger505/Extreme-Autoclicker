27896b3791b875007aafcbb4caa0fd4e213b569b
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-29 15:28:42 +0100

    Create header file for pointer

--- include/pointer.h
+++ include/pointer.h
@@ -1,3 +1,20 @@
 #pragma once
 
+#include <stdint.h>
 
+
+typedef struct {
+    uint32_t pressDuration;
+    uint32_t delay;
+    uint32_t maxClicks;
+} PointerSettings;
+
+void pointer_init();
+
+void pointer_set_settings(PointerSettings settings);
+
+void pointer_start();
+
+void pointer_stop();
+
+void pointer_destroy();
