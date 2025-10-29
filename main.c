1ff8be3b6c3f6f3d67fb88a4e24db2dd958ad486
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-29 13:10:43 +0100

    Start creating ui

--- main.c
+++ main.c
@@ -8,7 +8,57 @@
 #include <time.h>
 #include <gtk/gtk.h>
 
-int main(void) {
+static void
+print_hello(GtkWidget *widget,
+            gpointer data) {
+    g_print("Hello World\n");
+}
+
+static void activate(GtkApplication *app,
+                     gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *button;
+
+    /* create a new window, and set its title */
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+
+    /* Here we construct the container that is going pack our buttons */
+    grid = gtk_grid_new();
+
+    /* Pack the container in the window */
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+
+    button = gtk_button_new_with_label("Button 1");
+    g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
+
+    /* Place the first button in the grid cell (0, 0), and make it fill
+     * just 1 cell horizontally and vertically (ie no spanning)
+     */
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Button 2");
+    g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
+
+    /* Place the second button in the grid cell (1, 0), and make it fill
+     * just 1 cell horizontally and vertically (ie no spanning)
+     */
+    gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Quit");
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+
+    /* Place the Quit button in the grid cell (0, 1), and make it
+     * span 2 columns.
+     */
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 1);
+
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc,
+         char **argv) {
     struct libevdev *vdev = NULL;
     struct libevdev_uinput *uidev = NULL;
 
@@ -47,7 +97,7 @@ int main(void) {
     printf("Press Ctrl+C to stop\n\n");
 
 
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 1; i++) {
         printf("Sending automatic left mouse click...\n");
 
         // Press
@@ -68,6 +118,14 @@ int main(void) {
 
     libevdev_uinput_destroy(uidev);
     libevdev_free(vdev);
-    return 0;
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
 
